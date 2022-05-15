66cd4a5778b54c829abf1600da7945eeba2e2370
Edmartt <47486245+Edmartt@users.noreply.github.com>
2022-05-14 23:16:58 -0500

    refactor: refactor for using dependencies injected in struct instead of instancing objects inside the handler

--- internal/users/transport/handlers.go
+++ internal/users/transport/handlers.go
@@ -25,6 +25,7 @@ type Handlers struct {
 	user models.User
 	logResponse LoginResponse
 	sigResponse SignupResponse
+	wrapper jwt.JWTWrapper
 
 }
 
@@ -36,31 +37,33 @@ func(h *Handlers) Login(w http.ResponseWriter, request *http.Request){
 		log.Println(requestError.Error())
 	}
 
+	json.Unmarshal(reqBody, &h.user)
 
-	user := &models.User{}
-
-	json.Unmarshal(reqBody, &user)
-
-	searchedUser := h.userRepo.Find(user.Username)
+	searchedUser := h.userRepo.Find(h.user.Username)
 
 
-	if searchedUser.Username == user.Username{
-		if hasher.CheckHash(searchedUser.Password, user.Password){
-			claims := jwt.Claims{}
+	if searchedUser.Username == h.user.Username{
+		if hasher.CheckHash(searchedUser.Password, h.user.Password){
 
-			newToken := claims.GenerateJWT(user.Username, 5)
+			newToken, err := h.wrapper.GenerateJWT(h.user.Username, 5)
 
+			if err != nil{
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			h.logResponse.Token = newToken
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(newToken)
+			json.NewEncoder(w).Encode(&h.logResponse)
 			return
 		}
 
-		w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(w).Encode("Wrong username or password")
 
 		return
 	}
-	w.WriteHeader(http.StatusNotFound)
+	w.WriteHeader(http.StatusUnauthorized)
 	json.NewEncoder(w).Encode("Username or Password Wrong")
 
 	return
@@ -68,13 +71,11 @@ func(h *Handlers) Login(w http.ResponseWriter, request *http.Request){
 
 func (h *Handlers)Signup(w http.ResponseWriter, request *http.Request){
 
-	user := models.User{}
-	
-	user.Id = uuid.NewString()
-	requestError := json.NewDecoder(request.Body).Decode(&user)
+	h.user.Id = uuid.NewString()
+	requestError := json.NewDecoder(request.Body).Decode(&h.user)
 
-	hashedPassword := hasher.ConvertToHash(user.Password)
-	user.Password = hashedPassword
+	hashedPassword := hasher.ConvertToHash(h.user.Password)
+	h.user.Password = hashedPassword
 
 	if requestError != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -82,7 +83,7 @@ func (h *Handlers)Signup(w http.ResponseWriter, request *http.Request){
 	}
 
 
-	h.userRepo.Create(user)
+	h.userRepo.Create(h.user)
 	w.WriteHeader(http.StatusCreated)
 	h.sigResponse.Status = "User Created"
 	json.NewEncoder(w).Encode(h.sigResponse)

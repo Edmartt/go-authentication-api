d168a2cbfbc11321d098bbd28b10958a8215e182
Edmartt <47486245+Edmartt@users.noreply.github.com>
2022-05-29 00:11:15 -0500

    update return as pointer to users model

--- internal/users/data/irepository.go
+++ internal/users/data/irepository.go
@@ -5,6 +5,6 @@ import "github.com/Edmartt/go-authentication-api/internal/users/models"
 var RepoAccessInterface IUserRepository
 
 type IUserRepository interface{
-	Find(id string) models.User
+	Find(id string) *models.User
 	Create(user models.User) string
 }

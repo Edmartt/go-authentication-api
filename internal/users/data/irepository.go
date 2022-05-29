c00be745b953889117ab334da3aeebaae8543186
Edmartt <47486245+Edmartt@users.noreply.github.com>
2022-05-29 00:10:39 -0500

    adds variable for accesing the Repository interface methods

--- internal/users/data/irepository.go
+++ internal/users/data/irepository.go
@@ -2,6 +2,8 @@ package data
 
 import "github.com/Edmartt/go-authentication-api/internal/users/models"
 
+var RepoAccessInterface IUserRepository
+
 type IUserRepository interface{
 	Find(id string) models.User
 	Create(user models.User) string

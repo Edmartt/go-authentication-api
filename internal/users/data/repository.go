69aff041c70336d211e9d1ad4dd33ae5400e4f11
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-04-01 02:56:39 -0500

    chore: updates users struct to pointer struct

--- internal/users/data/repository.go
+++ internal/users/data/repository.go
@@ -2,61 +2,60 @@ package data
 
 import (
 	"log"
+
 	"github.com/Edmartt/go-authentication-api/internal/database"
 	"github.com/Edmartt/go-authentication-api/internal/users/models"
 )
 
-type UserRepository struct{
-	DB database.IConnection
-	user models.User
+type UserRepository struct {
+	DB   database.IConnection
+	user *models.User
 }
 
-
-func newRepository() database.IConnection{
+func newRepository() database.IConnection {
 	db := UserRepository{
 		DB: database.SQLite{},
 	}
 	return db.DB
 }
 
-
-func (data UserRepository) Find(username string) (*models.User){
+func (data UserRepository) Find(username string) *models.User {
 	connection, connError := newRepository().GetConnection()
-	if connError != nil{
+	if connError != nil {
 		log.Println("connection error")
 	}
-	
+
 	connection.Where("username = ?", username).First(&data.user)
-	
-	if data.user.Id == ""{
-		return &data.user
+
+	if data.user.Id == "" {
+		return data.user
 	}
 
-	return &data.user
+	return data.user
 }
 
-func (data UserRepository) Create(user models.User) string{
+func (data UserRepository) Create(user *models.User) string {
 	connection, connError := newRepository().GetConnection()
-	if connError != nil{
+	if connError != nil {
 		log.Println("connection error")
 	}
 	connection.Create(&user)
 	return user.Id
 }
 
-func (data UserRepository) Update(user models.User){
+func (data UserRepository) Update(user models.User) {
 	connection, connError := data.DB.GetConnection()
 
-	if connError != nil{
+	if connError != nil {
 		log.Println("connection error")
 	}
 	connection.Save(&user)
 }
 
-func (data UserRepository) Delete(user models.User, id string){
+func (data UserRepository) Delete(user models.User, id string) {
 	connection, connError := data.DB.GetConnection()
 
-	if connError != nil{
+	if connError != nil {
 		log.Println("connection error")
 	}
 	connection.Delete(&user, id)
